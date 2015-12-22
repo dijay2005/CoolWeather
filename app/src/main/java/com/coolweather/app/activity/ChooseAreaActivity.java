85656e9c9a2dbfc6bf02a25b214c14c2673d1d58
dijay2005_local <dijay2005@163.com>
2015-12-22 21:00:01 +0800

    加入显示天气信息的功能。

--- app/src/main/java/com/coolweather/app/activity/ChooseAreaActivity.java
+++ app/src/main/java/com/coolweather/app/activity/ChooseAreaActivity.java
@@ -2,7 +2,10 @@ package com.coolweather.app.activity;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
@@ -72,6 +75,14 @@ public class ChooseAreaActivity extends Activity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if (prefs.getBoolean("city_selected", false))
+        {
+            Intent intent = new Intent(this, WeatherActivity.class);
+            startActivity(intent);
+            finish();
+            return;
+        }
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.choose_area);
         listView = (ListView) findViewById(R.id.list_view);
@@ -92,6 +103,13 @@ public class ChooseAreaActivity extends Activity
                 {
                     selectedCity = cityList.get(position);
                     queryCountries();
+                }else if (currentLevel ==LEVEL_COUNTRY)
+                {
+                    String countryCode = countryList.get(position).getCountryCode();
+                    Intent intent = new Intent(ChooseAreaActivity.this, WeatherActivity.class);
+                    intent.putExtra("country_Code", countryCode);
+                    startActivity(intent);
+                    finish();
                 }
             }
         });
